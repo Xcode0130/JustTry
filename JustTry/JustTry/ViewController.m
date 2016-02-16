50577dfcecc20585fe68f1e54ea182ba8aeba549
Demo <DEMO@JL.Local>
2016-02-16 13:35:54 +0800

    测试

--- JustTry/JustTry/ViewController.m
+++ JustTry/JustTry/ViewController.m
@@ -28,6 +28,7 @@
     });
 //    [self.view addSubview:lblShow];
     
+    //test
     UIButton * btnTest = ({
         UIButton * btn = [[UIButton alloc] initWithFrame:CGRectMake(40, 44, [UIScreen mainScreen].bounds.size.width - 80, 44)];
         [btn addTarget:self action:@selector(Test:) forControlEvents:UIControlEventTouchUpInside];

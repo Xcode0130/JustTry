e9a44b4b85c35f83fb75b420d6ab2e316ba47051
DEMO0130 <jl.0130@hotmail.com>
2016-05-19 20:01:53 +0800

    sf

--- JustTry/JustTry/AppDelegate.m
+++ JustTry/JustTry/AppDelegate.m
@@ -14,7 +14,8 @@
 
 @implementation AppDelegate
 
-
+// test
+//test
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
